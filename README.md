a0fd9c4969ebd47b3c4e363108308ae50978ae53
Qanysh <220667@astanait.edu.kz>
2024-02-22 16:52:40 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# AITU | Arsen Zhamshitov | SE-2203
+# WEB Technologies 2 (Back End)
+# Assignment 4
+
+## Installation
+To run this project, install it locally using npm:
+
+```
+$ npm install
+$ npm start
+```
---
+++ server/api.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+const TRIPADVISOR_API = '7494c0f261mshb1225b4c3397e73p1a620fjsnabd669d86a1b'
+
+
+// async function getRestaurants() {
+//     let response, responseData = null;
+
+//     try {
+//        response await = axios.get(``);
+//     }
+// }
